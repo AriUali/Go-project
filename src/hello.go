1aed4868a35a8167dad6910cc90503267b3b19cb
AriUali <91005723+AriUali@users.noreply.github.com>
2023-02-15 19:11:30 +0600

    Add files via upload

---
+++ src/hello.go
@@ -0,0 +1,65 @@
+
+
+// package main
+
+// import "fmt"
+
+// func main() {
+//     // определите переменные ver, id, pi
+//     // ...
+//     cv := (2 % 7)
+//     ver := "v0.0.1"
+//     id := 0
+//     pi := 3.1415
+//     fmt.Println("ver =", ver, "id =", id, "pi =", pi)
+//     fmt.Println(Black != Gray, cv )
+
+//     fmt.Println(one, three, five, seven, nine, eleven)
+
+//     a := 0.10000001 // float64
+// // инициализация и основное условие
+// if b := float32(a); b > float32(0.1) {
+//     fmt.Println("Var a is GT float32(0.1)")
+// }
+
+// numberr := 2
+// switch b := numberr % 5; {
+// case b == 0:
+//     fmt.Println("Кратно 5")
+// default:
+//     fmt.Printf("Остаток от деления на 5: %d", b)
+// }
+
+// fmt.Println()
+
+// check := -100
+// switch {
+// case check > 0:
+//     if check % 2 == 0 {
+//         break
+//     }
+//     fmt.Println("Odd positive value received")
+// case check < 0:
+//     fmt.Println("Negative value received")
+//     fallthrough
+// default:
+//     fmt.Println("Default value handling")
+// }
+
+// }
+
+// const(
+//     Black = "Black"
+//     Gray = "Gray"
+//     Blue = "Blue"
+// )
+
+// const (
+//     one =  2* iota + 1
+//     three
+//     five
+//     seven
+//     nine
+//     eleven
+// )
+
